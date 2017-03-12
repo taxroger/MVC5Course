8ff2433bb15bc719133dd1960c56c5fc5325bf5b
taxroger <taxroger@gmail.com>
2017-03-12 13:30:24 +0800

    Entity Framework partial class

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,32 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [Required(ErrorMessage = "請輸入欄位值({0})!")]
+        [DisplayName("產品名稱")]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(10, 99999, ErrorMessage = "金額設定錯誤!")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,9 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,18 +19,11 @@ namespace MVC5Course.Models
         {
             this.OrderLine = new HashSet<OrderLine>();
         }
-
+    
         public int ProductId { get; set; }
-        [Required(ErrorMessage = "請輸入欄位值({0})!")]
-        [DisplayName("產品名稱")]
         public string ProductName { get; set; }
-        [Required]
-        [Range(10, 99999, ErrorMessage = "金額設定錯誤!")]
-        //[DisplayFormat(DataFormatString = "{0:N0}")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
