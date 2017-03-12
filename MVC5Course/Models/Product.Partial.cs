265d412cd505473b1adc51713481362edc1dfe3d
taxroger <taxroger@gmail.com>
2017-03-12 14:22:41 +0800

    自訂Validation Attribute

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -4,6 +4,7 @@ namespace MVC5Course.Models
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
+    using Validations;
 
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
@@ -18,6 +19,7 @@ namespace MVC5Course.Models
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         [Required(ErrorMessage = "請輸入欄位值({0})!")]
         [DisplayName("產品名稱")]
+        [NoWill(ErrorMessage ="不能有Will字串")]
         public string ProductName { get; set; }
         [Required]
         [Range(10, 99999, ErrorMessage = "金額設定錯誤!")]
---
+++ MVC5Course/Models/Validations/NoWill.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.Validations
+{
+    public class NoWill : DataTypeAttribute
+    {
+        public NoWill() : base(DataType.Text)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            string str = Convert.ToString(value);
+
+            if (str.Contains("Will"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
