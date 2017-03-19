ff9ac0fb3d1bfb9b70083e5e72381ba4281b6c71
taxroger <taxroger@gmail.com>
2017-03-19 13:38:31 +0800

    razor test

---
+++ MVC5Course/Controllers/RTController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class RTController : Controller
+    {
+        // GET: RT
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RazorTest()
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+
+            return PartialView(data);
+        }
+    }
+}
